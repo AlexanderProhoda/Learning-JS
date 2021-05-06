2f9bb93c206e7c5d5f061323ef387be4dfd884b9
AlexanderProhoda <qudee96@gmail.com>
2021-05-06 21:15:00 +0300

    types test

---
+++ main.js
@@ -0,0 +1,7 @@
+let name = "Yoba";
+//Что выведет этот скрипт? Приведу значения
+alert( `hello ${1}` ); // ошибкка
+
+alert( `hello ${"name"}` ); // hello Yoba
+
+alert( `hello ${name}` ); // ошибка
