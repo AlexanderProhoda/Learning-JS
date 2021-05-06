80911b6117add1724e4b02109577ec9511ce6efa
AlexanderProhoda <qudee96@gmail.com>
2021-05-06 21:27:53 +0300

    убираю файлы

--- main.js
+++
@@ -1,7 +0,0 @@
-let name = "Yoba";
-//Что выведет этот скрипт? Приведу значения
-alert( `hello ${1}` ); // ошибкка
-
-alert( `hello ${"name"}` ); // hello Yoba
-
-alert( `hello ${name}` ); // ошибка
