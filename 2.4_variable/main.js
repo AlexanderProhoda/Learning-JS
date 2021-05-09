41632cf186da47cd9d445cba3786a203f318daf1
AlexanderProhoda <qudee96@gmail.com>
2021-05-09 17:12:41 +0300

    rebuilding tree

---
+++ 2.4_variable/main.js
@@ -0,0 +1,13 @@
+// task1
+let admin;
+let firstName = 'Jhon';
+
+admin = firstName;
+
+alert(admin);
+
+// task2
+let planet = "Earth";
+let newLocalUser; // let currentUserName = "Джон";
+
+// task3
