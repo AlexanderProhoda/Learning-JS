f1f05181e296492579f8c1df792f872377effbd3
AlexanderProhoda <qudee96@gmail.com>
2021-05-11 19:18:17 +0300

    операторы сравнения

---
+++ 2.9_RelationOp/main.js
@@ -0,0 +1,17 @@
+//Каким будет результат этих выражений?
+
+5 > 4 //true
+"ананас" > "яблоко" //true - тут ошибся, просто не подумав
+"2" > "12" //true
+undefined == null //true
+undefined === null //false
+null == "\n0\n" //false
+null === +"\n0\n" //false
+
+// 5 > 4 → true
+// "ананас" > "яблоко" → false
+// "2" > "12" → true
+// undefined == null → true
+// undefined === null → false
+// null == "\n0\n" → false
+// null === +"\n0\n" → false
